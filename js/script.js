b92d297bcaceb3dba535a1c4019138cc3c7e6ba1
Niklas Schoenberger <nik.schoe@icloud.com>
2017-03-18 17:15:26 +0100

    first commit - Home & Clothes fin

--- js/script.js
+++ js/script.js
@@ -3,3 +3,16 @@ $(document).on('click', '.scroll-btn', function(event){
     $('html, body').animate({
         scrollTop: $( $.attr(this, 'href') ).offset().top}, 850);
 });
+
+
+    $(window).on('load', (function() {
+        var mainPoint = $("#main").offset().top;
+        $(window).scroll(function() {
+            if ($(window).scrollTop() >= mainPoint) {
+                $('.home-menu').fadeIn(100);
+            } else {
+                $('.home-menu').fadeOut(100);
+            }
+        });
+     }));
+
