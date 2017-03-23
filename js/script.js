ce48881567034643d9db2a7123dc2e91f713642d
Niklas Schoenberger <nik.schoe@icloud.com>
2017-03-23 17:01:34 +0100

    lost count_2 -m

--- js/script.js
+++ js/script.js
@@ -18,27 +18,96 @@
             }
         });
     });
-    
-   /* $(document).ready(function () {
-        $('.icon-menu').on('click', function () {
-            $('.nav-list-mobile').slideToggle(250);
-        })
-    });*/
 
-
+    // BURGER MENU ANIMATION
     $(document).ready(function(){
-        var $burgerico = $('#nav-icon3');
-        $burgerico.click(function(){
-            $burgerico.toggleClass('open');
+        var $burgerico = $('#nav-icon3'); // select burger icon
+        $burgerico.click(function () { // give burger icon click event
+            $burgerico.toggleClass('open'); // toggle menu to x animation
             $('.nav-list-mobile').slideToggle(250);
+            // toggle menu (hide and display slide down animation)
         });
+    });
+
+
+    // CLOTHES SLIDER
+    $(document).ready(function(){
+        var $showslide = $('.show-slides');
+        var $slider = $('.slider');
+
+
+        var $slideSwitch = function() {
+
+
+            var $active = $('.slide-list li.active');
+            var $next = $active.next();
+
+            console.log($active)
+
+                if($active.length > 0) {
+                    $next.addClass('active');
+                    $active.removeClass('active');
+                } else {
+                    $('.slide-list li').first().addClass('active');
+                }
+
+
+        }
+
+        var $slideshow = function() {
+            setInterval($slideSwitch, 2500);
+        };
+
+
+        $showslide.on('click', function () {
+            $slider.slideToggle(250);
+            $slideshow();
+
+        });
+
     });
 
 
 
 
 
+    $(document).ready(function(){
 
+        $('#myform').on('submit', function (_e) {
+
+
+            var $errormsg = $('<span></span>');
+            var $errorcnt = $('.error-cnt');
+
+
+            if($('#name').val().trim() === "") {
+                $errorcnt.prepend($errormsg.textContent="Pflichtfeld");
+            }
+
+
+
+
+
+
+
+
+
+
+            _e.preventDefault();
+
+
+
+
+
+
+        });
+
+
+
+
+
+
+    });
 
 
 
